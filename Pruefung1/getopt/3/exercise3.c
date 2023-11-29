962410c283fbe544de00cdb39fc826c42809a91f
chrisvenator <55980003+Chrisvenator@users.noreply.github.com>
2023-11-29 15:51:14 +0100

    exercises

--- Pruefung1/getopt/3/exercise3.c
+++ Pruefung1/getopt/3/exercise3.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <getopt.h>
 
 /** name of the executable (for printing messages) */
 char *program_name = "<not yet set>";
@@ -18,14 +19,61 @@ int main(int argc, char **argv) {
     program_name = argv[0];
   }
 
+  char * a = "false";
+  bool b_set = false;
+  char * c = "false";
+
   // ./client {-a optargA | -b | -c [optargC] } file... (exactly 4 files)
   // - set flag, if -b occured
   // - add all four files in reversed mentioned-order to totalString
+
+int opt;
+while ((opt = getopt(argc, argv, "a:bc::")) != -1) {
+    switch (opt) {
+    case 'a':
+        if (strncmp(a, "false", 4) || b_set == true || strncmp(c, "false", 4)) usage("a");
+
+        a = optarg;
+        break;
+    case 'b':
+        if (strncmp(a, "false", 4) || b_set == true || strncmp(c, "false", 4)) usage("b");
+
+        b_set = true;
+        break;
+    case 'c':
+        if (strncmp(a, "false", 4) || b_set == true || strncmp(c, "false", 4)) usage("c");
+        
+        c = "true";
+        if (optarg){
+          c = argv[optind];
+          optind++;
+        }
+        break;
+    default: /* '?' */
+        usage("default");
+        assert(0);
+        break;
+    }
+}
+
+if (argc - optind != 4) usage("files");
+
+char * total_string = malloc(4 * sizeof(char));
+
+for (size_t i = 0; i < 4; i++){
+  strcat(total_string, argv[optind]);
+  optind++;
+}
+
+
+
 
   printf("a: %s\n", a);
   printf("b_set: %s\n", b_set ? "true" : "false");
   printf("c: %s\n", c);
   printf("total_string: %s\n", total_string);
+
+  free(total_string);
 
   return 0;
 }
